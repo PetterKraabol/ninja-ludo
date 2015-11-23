eb56fbf51e8ad37e8d1ead7b68ff366937b1f228
Petter Kraabøl <petter.zarlach@gmail.com>
2015-11-24 00:17:19 +0100

    LOGINDENIED fix

--- src/com/ludo/server/ChatServer.java
+++ src/com/ludo/server/ChatServer.java
@@ -108,7 +108,6 @@ public class ChatServer {
                                 break;
                             } else {
                                 out.println("LOGINDENIED");
-                                return;
                             }
                         }
                     }
