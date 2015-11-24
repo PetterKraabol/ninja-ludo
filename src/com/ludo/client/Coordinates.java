15c9a64a3ba30071835e9336b788ad1d2bee96eb
Gudbrand Grini <you@example.com>
2015-11-24 14:43:28 +0100

    Lagt til kordinater for alle feltene for det normale brette, samt for det foskjellige fargene og indikator for hven sin tur det indikatoren

---
+++ src/com/ludo/client/Coordinates.java
@@ -0,0 +1,19 @@
+package com.ludo.client;
+
+public class Coordinates {
+	public int X;
+	public int Y;
+	
+	public Coordinates(int tmpX, int tmpY) {
+		this.X = tmpX;
+		this.Y = tmpY;
+	}
+	
+	public int getXCordinates() {
+		return this.X;
+	}
+	
+	public int getYCordinates() {
+		return this.Y;
+	}
+}
--- src/com/ludo/client/controllers/GameController.java
+++ src/com/ludo/client/controllers/GameController.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.ludo.client.Coordinates;
+
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -91,20 +94,113 @@ public class GameController implements Initializable{
 	// color variable
 	private String color = "red"; 
 	
+	private Coordinates[] fields;
+	private Coordinates[] indicator;
+	
 	public void selectBrick(MouseEvent event) throws IOException {
 		
 		if (color.equals("red")) {
 			System.out.println("X: " + ((Node) event.getSource()).getLayoutX());
 			System.out.println("Y: " + ((Node) event.getSource()).getLayoutY());
 			
-			redBrick1.setLayoutX(250);
+			indicator = new Coordinates[4];
+			indicator[1] = new Coordinates(225, 225);	// Red indicator
+			indicator[2] = new Coordinates(405, 225);	// Blue indicator
+			indicator[3] = new Coordinates(405, 405);	// Yellow indicator
+			indicator[4] = new Coordinates(225, 405);	// Green indicator
+			
+			// Start is the first red field and the goes the same was as when you play the game
+			fields = new Coordinates[76];
+			fields[1]  = new Coordinates(63, 272);	// Red field
+			fields[2]  = new Coordinates(105, 272);
+			fields[3]  = new Coordinates(146, 272);
+			fields[4]  = new Coordinates(188, 272);
+			fields[5]  = new Coordinates(230, 272);
+			fields[6]  = new Coordinates(272, 230);
+			fields[7]  = new Coordinates(272, 189);
+			fields[8]  = new Coordinates(272, 147);
+			fields[9]  = new Coordinates(272, 105);
+			fields[10] = new Coordinates(272, 63);
+			fields[11] = new Coordinates(272, 22);
+			fields[12] = new Coordinates(314, 22);
+			fields[13] = new Coordinates(356, 22);
+			fields[14] = new Coordinates(356, 63);	// Blue field
+			fields[15] = new Coordinates(356, 105);
+			fields[16] = new Coordinates(356, 147);
+			fields[17] = new Coordinates(356, 189);
+			fields[18] = new Coordinates(356, 230);
+			fields[19] = new Coordinates(398, 272);
+			fields[20] = new Coordinates(440, 272);
+			fields[21] = new Coordinates(482, 272);
+			fields[22] = new Coordinates(524, 272);
+			fields[23] = new Coordinates(566, 272);
+			fields[24] = new Coordinates(608, 272);
+			fields[25] = new Coordinates(608, 315);
+			fields[26] = new Coordinates(608, 357);
+			fields[27] = new Coordinates(566, 357);	// Yellow field
+			fields[28] = new Coordinates(524, 357);
+			fields[29] = new Coordinates(482, 357);
+			fields[30] = new Coordinates(440, 357);
+			fields[31] = new Coordinates(398, 357);
+			fields[32] = new Coordinates(356, 400);
+			fields[33] = new Coordinates(356, 441);
+			fields[34] = new Coordinates(356, 483);
+			fields[35] = new Coordinates(356, 525);
+			fields[36] = new Coordinates(356, 567);
+			fields[37] = new Coordinates(356, 608);
+			fields[38] = new Coordinates(314, 608);
+			fields[39] = new Coordinates(272, 608);
+			fields[40] = new Coordinates(272, 567);	// Green field
+			fields[41] = new Coordinates(272, 525);
+			fields[42] = new Coordinates(272, 483);
+			fields[43] = new Coordinates(272, 441);
+			fields[44] = new Coordinates(272, 440);
+			fields[45] = new Coordinates(230, 357);
+			fields[46] = new Coordinates(188, 357);
+			fields[47] = new Coordinates(146, 357);
+			fields[48] = new Coordinates(105, 357);
+			fields[49] = new Coordinates(63, 357);
+			fields[50] = new Coordinates(21, 357);
+			fields[51] = new Coordinates(21, 315);
+			fields[52] = new Coordinates(21, 272);
+			
+			// Color field after normal road - Red
+			fields[53] = new Coordinates(63, 315);	// Color field for red second field
+			fields[54] = new Coordinates(105, 315);
+			fields[55] = new Coordinates(146, 315);
+			fields[56] = new Coordinates(188, 315);
+			fields[57] = new Coordinates(230, 315);
+			fields[58] = new Coordinates(270, 332);	// Arrow red - 7 på Y
+			
+			// Color field after normal road - Blue
+			fields[59] = new Coordinates(314, 63);	// Color field for blue second field
+			fields[60] = new Coordinates(314, 105);
+			fields[61] = new Coordinates(314, 147);
+			fields[62] = new Coordinates(314, 189);
+			fields[63] = new Coordinates(314, 230);
+			fields[64] = new Coordinates(297, 271);	// Arrow blue + 7 på X
+			
+			// Color field after normal road - Yellow
+			fields[65] = new Coordinates(566, 315);	// Color field for yellow second field
+			fields[66] = new Coordinates(524, 315);
+			fields[67] = new Coordinates(482, 315);
+			fields[68] = new Coordinates(440, 315);
+			fields[69] = new Coordinates(398, 315);
+			fields[70] = new Coordinates(558, 298);	// Arrow yellow + 7 på Y
+			
+			// Color field after normal road - Yellow
+			fields[71] = new Coordinates(314, 567);	// Color field for yellow second field
+			fields[72] = new Coordinates(314, 525);
+			fields[73] = new Coordinates(314, 483);
+			fields[74] = new Coordinates(314, 441);
+			fields[75] = new Coordinates(314, 400);
+			fields[76] = new Coordinates(331, 359);	// Arrow yellow - 7 på X
 		}
 	}
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		
+			
 	}
 
 }
